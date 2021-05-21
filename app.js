f5d6d92392d4f714fad2bfcb8ac5ed1cdd73dcb5
jmcia2020 <jeanmmcmahon@gmail.com>
2021-05-20 22:51:34 -0500

    add event listener @click to button. add fetch await on api and constructor function.

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const app = Vue.createApp({
     }
   },
 
-  /*
+
   methods: {
     async getUser() {
       const res = await fetch('https://randomuser.me/api')
       const { results } = await res.json()
 
-      // console.log(results)
+      console.log(results)
 
       this.firstName = results[0].name.first
       this.lastName = results[0].name.last
@@ -24,8 +24,7 @@ const app = Vue.createApp({
       this.gender = results[0].gender
       this.picture = results[0].picture.large
     },
-  },
-  */
+  },     
 })
 
 app.mount('#app')
